506076558ee40a31015504a0eb68f203a8b6b28b
zhaosiyuan <zhaosiyuan@MacBook-Pro.local>
2024-07-05 17:09:22 +0800

    为timeout增加类型

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,9 +12,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   function debounce(callback: Function, wait: number) {
     // 用来保存定时器任务的标识id
-    let timeoutId = null
+    let timeoutId: any = null
     // 返回一个事件监听函数(也就是防抖函数)
-    return function (event) {
+    return function (event: any) {
       console.log(timeoutId, event)
       // 清除未执行的定时器任务
       if (timeoutId) {
